15332a87d33027d31e60e60cf157e5cef1a11380
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2023-03-19 16:08:24 +0330

    add nodemon and edit package.json

--- less-3-node/index.js
+++ less-3-node/index.js
@@ -3,7 +3,7 @@ const http = require("http");
 http.createServer((req,res)=>{
     console.log(req);
     res.writeHead(200,{"content-type":"text/plain"})
-    res.end("Hello NodeJs")
+    res.end("Hello NodeJs Hello")
 }).listen(3000,()=>{
     console.log("server run on port 3000 : http://localhost:3000");
 });
