2cb3033cc9724049f52c7280a2984253287ba3a0
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2023-03-19 15:37:42 +0330

    end of less-3-node

---
+++ less-3-node/index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+http.createServer((req,res)=>{
+    console.log(req);
+    res.writeHead(200,{"content-type":"text/plain"})
+    res.end("Hello NodeJs")
+}).listen(3000,()=>{
+    console.log("server run on port 3000 : http://localhost:3000");
+});
